6f77d97fa7d62e178d6f6bdac673d90a2d814a28
Vinicius F <viniciusvfs@hotmail.com>
2016-10-17 08:59:45 -0200

    Comecando a implementar o tabuleiro

--- Xad/Program.cs
+++ Xad/Program.cs
@@ -10,12 +10,14 @@ namespace Xad
     {
         static void Main(string[] args)
         {
+            Tabuleiro tab = new Tabuleiro(8, 8);
+            /*
             Posicao P;
 
             P = new Posicao(3, 4);
 
             Console.WriteLine("Posicao: " + P);
-
+            */
             Console.ReadLine();
         }
     }
