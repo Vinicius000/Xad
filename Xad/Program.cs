510b6343ca091ca46632679d01678c6535d4a390
Vinicius F <viniciusvfs@hotmail.com>
2016-10-17 09:13:57 -0200

    Comecando a implementar um metodo que imprime o tabuleiro

--- Xad/Program.cs
+++ Xad/Program.cs
@@ -11,6 +11,8 @@ namespace Xad
         static void Main(string[] args)
         {
             Tabuleiro tab = new Tabuleiro(8, 8);
+
+            Tela.imprimirTabuleiro(tab);
             /*
             Posicao P;
 
